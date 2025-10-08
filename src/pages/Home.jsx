8ba25a2c835de7e2ced6676992ad30d38b6bd655
quanghoatrinh <quanghoa.trinh@gmail.com>
2025-10-08 20:52:42 +0700

    react bootstrap

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,33 @@
+import { Carousel } from "react-bootstrap";
 function Home(){
     return (
         <div className="container">
             <h1>Home Page</h1>
+            <Carousel>
+                <Carousel.Item>
+                    <img src="/images/blue_ocean.jpg" />
+                    <Carousel.Caption>
+                    <h3>First slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img src="/images/blue2.jpg" />
+                    <Carousel.Caption>
+                    <h3>Second slide label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img src="/images/landscape.jpg" />
+                    <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>
+                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                    </p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                </Carousel>
         </div>
     )
 }
